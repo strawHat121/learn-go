6713b406a007757b690851e698e7c2e115b148db
Kinjalk <kinjalkbajpai@gmail.com>
2024-03-02 16:09:47 +0530

    hello world in go

---
+++ hello-world/go.mod
@@ -0,0 +1,3 @@
+module github.com/strawHat121/hello-world-go
+
+go 1.20
---
+++ hello-world/hello_test.go
@@ -0,0 +1,34 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello world when string is empty", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
